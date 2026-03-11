1ea5fb51ce8513be4bd598b4a982282836f8ebe0
BangkitBayu <martinusmartinussaja@gmail.com>
2026-03-11 10:17:11 +0700

    push package.json package-lock.json documents

--- cek.js
+++
@@ -1,1 +0,0 @@
-console.log(typeof process.env.WA_NUMBER)
--- index.js
+++ index.js
@@ -27,25 +27,33 @@ setInterval(
   15 * 60 * 1000, // Kosongkan variabel setiap 15 menit
 );
 
+let isSendPairingCode = false;
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
   const sock = makeWASocket({
     auth: state,
-    logger: pino({ level: "info" }),
-    browser: ["Windows", "Chrome", "10.0"],
+    logger: pino({ level: "silent" }),
   });
 
   sock.ev.on("creds.update", saveCreds);
 
   sock.ev.on("connection.update", async (update) => {
     const { connection, lastDisconnect } = update;
-
+    console.log(update);
     if (connection === "close") {
       const shouldReconnect =
         lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
-      console.log(`Connection failed`);
+
+      // console.log(`Connection failed`);
+
       if (shouldReconnect) {
-        startBot()
+        try {
+          setTimeout(() => {
+            startBot();
+          }, 5000);
+        } catch (error) {
+          console.log("Failed reconnecting:", error);
+        }
       }
     }
     if (connection === "open") {
@@ -78,61 +86,66 @@ async function startBot() {
       }
     }
 
-    if (connection === "connecting" && !sock.authState.creds.registered) {
-      console.log("pairing code");
+    if (
+      connection === "connecting" &&
+      !sock.authState.creds.registered &&
+      !isSendPairingCode
+    ) {
+      isSendPairingCode = true;
       try {
-        setTimeout(async () => {
-          let code = await sock.requestPairingCode(waNumber);
-          console.log("Pairing code:", code);
-        }, 3000);
+        // setTimeout(async () => {
+        await delay(10000);
         console.log("Requesting pairing code ...");
+        let code = await sock.requestPairingCode(waNumber);
+        console.log("Pairing code:", code);
+        // }, 3000);
       } catch (error) {
         console.log("Failed to request pairing code:", error);
       }
     }
-    sock.ev.on("messages.upsert", async ({ messages, type }) => {
-      if (type === "notify") {
-        for (let message of messages) {
-          const msgId = message.key.id;
+  });
+  sock.ev.on("messages.upsert", async ({ messages, type }) => {
+    if (type === "notify") {
+      for (let message of messages) {
+        const msgId = message.key.id;
 
-          if (repliesHistory.has(msgId)) return;
+        if (repliesHistory.has(msgId)) return;
 
-          repliesHistory.add(msgId);
+        repliesHistory.add(msgId);
 
-          console.log(repliesHistory);
+        console.log(repliesHistory);
 
-          if (!message.key.fromMe && message.message) {
-            const sender = message.key.remoteJid;
+        if (!message.key.fromMe && message.message) {
+          const sender = message.key.remoteJid;
 
-            const reply =
-              message.message.conversation ||
-              message.message.extendedTextMessage?.text;
+          const reply =
+            message.message.conversation ||
+            message.message.extendedTextMessage?.text;
 
-            console.log(
-              `Received message from ${sender}: ${reply}, id: ${message.key.id}`,
-            );
+          console.log(
+            `Received message from ${sender}: ${reply}, id: ${message.key.id}`,
+          );
 
-            // Handle recipient mengirim mau
-            if (reply?.toLowerCase() === "mau" || reply?.includes("mau")) {
-              const imagePath = "./media/img/brosur.jpg";
-              await sock.sendMessage(sender, {
-                image: {
-                  url: imagePath,
-                },
-                caption:
-                  "Berikut ini adalah brosur dari program Goloka, untuk pendaftaran program dan info lebih lanjut, silahkan hubungi tim kami: *Rangga Pratama Putra:6285189081947*. Terimakasih atas waktu dan perhatian anda😊🙏",
-              });
-            } else {
-              const text =
-                "Silahkan balas *MAU* untuk info lebih lanjut dari program Goloka dan anda akan segera terhubung dengan tim kami. Terimkasih atas waktu dan perhatian anda😊🙏.";
-              await sock.sendMessage(sender, {
-                text: text,
-              });
-            }
+          // Handle recipient mengirim mau
+          if (reply?.toLowerCase() === "mau" || reply?.includes("mau")) {
+            const imagePath = "./media/img/brosur.jpg";
+            await sock.sendMessage(sender, {
+              image: {
+                url: imagePath,
+              },
+              caption:
+                "Berikut ini adalah brosur dari program Goloka, untuk pendaftaran program dan info lebih lanjut, silahkan hubungi tim kami: *Rangga Pratama Putra:6285189081947*. Terimakasih atas waktu dan perhatian anda😊🙏",
+            });
+          } else {
+            const text =
+              "Silahkan balas *MAU* untuk info lebih lanjut dari program Goloka dan anda akan segera terhubung dengan tim kami. Terimkasih atas waktu dan perhatian anda😊🙏.";
+            await sock.sendMessage(sender, {
+              text: text,
+            });
           }
         }
       }
-    });
+    }
   });
 }
 
